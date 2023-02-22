75e50a3d5d5dae8fccb72854720b14f52ec6d7e4
Hashenshing <juanpablo89correa@gmail.com>
2023-02-22 15:59:39 -0500

    Class Date.cs created

---
+++ WebAppConcepts/WebAppConcepts/Date.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAppConcepts
+{
+    public class Date
+    {
+
+
+        #region
+        private int _year;
+        private int _month;
+        private int _day;
+
+
+        #endregion
+
+
+
+        #region Methods
+
+        //ctor
+
+        public Date()
+        {
+                
+        }
+
+
+        public Date(int year, int month, int day)
+        {
+            _year = CheckYear(year);
+            _month = CheckMonth(month);
+            _day = CheckDay(day, month, year);
+
+        }
+
+        private int CheckYear(int year)
+        {
+            if(year >= 1900)
+            {
+                return year;
+            }
+            throw new YearException("The year is invalid");
+        }
+
+        private int CheckDay(int day, int month, int year)
+        {
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+
+            }
+            int[] daysPorMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (day >= 1 && day <= daysPorMonth[month])
+            {
+                return day;
+
+            }
+
+
+            throw new DayException("The day is invalid");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+
+            }
+            throw new MonthException("The month is invalid");
+        }
+
+        public override string ToString()
+        {
+            return $"{_year}/{_month}/{_day}";
+        }
+
+
+        #endregion
+
+
+
+
+
+
+
+    }
+}
---
+++ WebAppConcepts/WebAppConcepts/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WebAppConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("*****************************");
+                Console.WriteLine("         Date Class");
+                Console.WriteLine("*****************************");
+                Console.WriteLine("");
+                Console.Write("Please type your year: ");
+                int year = Int16.Parse(Console.ReadLine());
+               
+                Console.WriteLine("");
+                Console.Write("Please type your month: ");
+                int month = Int16.Parse(Console.ReadLine());
+                
+                Console.WriteLine("");
+                Console.Write("Please type your day: ");
+                int day = Int16.Parse(Console.ReadLine());
+                
+                Console.WriteLine("");
+                Date dateObject = new Date(year, month, day);
+                
+                Console.WriteLine("The date entereer is: " + dateObject);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+    }
+}
